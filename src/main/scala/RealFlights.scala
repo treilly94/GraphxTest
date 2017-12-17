2a79ffc6726eb58abf31a1184ceab0969152f149
treilly <treilly94@live.com>
2017-12-17 23:12:20 +0000

    created graph

--- src/main/scala/RealFlights.scala
+++ src/main/scala/RealFlights.scala
@@ -1,4 +1,4 @@
-import org.apache.spark.graphx.VertexId
+import org.apache.spark.graphx.{Edge, Graph, VertexId}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -11,8 +11,7 @@ object RealFlights {
     // Make graph
     val graph = getGraph(spark)
 
-    graph.printSchema()
-    graph.show()
+    graph.triplets.foreach(println(_))
   }
 
 
@@ -35,19 +34,23 @@ object RealFlights {
       .drop("_c17")
     // Get unique airports
     val airportsRdd: RDD[(VertexId, String)] =
-      flights.rdd
+      flights
+        .rdd
         .map(row => (row(5).asInstanceOf[Number].longValue, row(6).asInstanceOf[String]))
           .union(flights
             .rdd
             .map(row => (row(7).asInstanceOf[Number].longValue, row(8).asInstanceOf[String]))
           ).distinct()
     // Get routes
-
+    val routesRdd: RDD[Edge[Long]] =
+      flights
+        .rdd
+        .map(row => Edge(row(5).asInstanceOf[Number].longValue,
+          row(7).asInstanceOf[Number].longValue,
+          row(16).asInstanceOf[Number].longValue))
     // Create graph
-    println("Airports")
-    airportsRdd.take(10).foreach(println(_))
-
-    flights
+    val default: String = "Missing Airport"
+    Graph(airportsRdd, routesRdd, default)
   }
 
 }
